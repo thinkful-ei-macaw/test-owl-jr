76b200b9e7025c4a3d379e2b7b79b2e745a17ee3
Raul Garcia <punkvomit21@gmail.com>
2020-03-03 17:31:16 -0500

    working on if statement

--- src/Card.js
+++ src/Card.js
@@ -6,7 +6,10 @@ export default function Card(props) {
     <div className='Card'>
       <img className="avatar" alt="User's avatar" src={props.imgsrc} />
       <p>{props.name}</p>
-      <p>{props.inSession ? "On Stage" : "Not on Stage"}</p>
+      <p className='session'>
+        {props.inSession ? "In session" : "Not in session"}
+      </p>
+      <p>{props.onStage ? "On Stage" : "Not on Stage"}</p>
     </div>
   )
 }
--- src/List.js
+++ src/List.js
@@ -5,15 +5,18 @@ import Card from './Card'
 function List(props) {
   return (
     <section className='participantList'>
-      {props.participants.map(person => {
-        return (
-          <Card
-            name={person.name}
-            imgsrc={person.avatar}
-            inSession={person.inSession}
-          />
-        );
-      })}
+      {person.inSession ? !props.person.push() :
+        {
+          props.participants.map(person => {
+            return (
+              <Card
+                name={person.name}
+                imgsrc={person.avatar}
+                inSession={person.inSession}
+              />
+            );
+          })
+        }
     </section>
   );
 }
